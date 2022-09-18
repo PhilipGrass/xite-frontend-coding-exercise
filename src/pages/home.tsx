333f0158e8fc8dc02156fbcdcd0b7c2ca46ec278
Phil <philip.grasselli@gmail.com>
2022-09-18 19:21:48 +0200

    Added pagination, spinner, fake timeout for fetching data and many other features

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,34 +1,49 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useMemo } from "react";
 import { Catalogue } from "../types/types";
 import axios, { AxiosResponse } from "axios";
-import { Spinner } from "react-bootstrap";
+import CustomSpinner from "../components/CustomSpinner";
 import CustomCard from "../components/CustomCard";
+import CustomPagination from "../components/CustomPagination";
 
 import styles from "../styles/pages/home.module.css";
+import CustomNavbar from "../components/CustomNavbar";
+
+const ITEMS_PER_PAGE = 20;
 
 const Home = () => {
-  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [currentPage, setCurrentPage] = useState<number>(1);
   const [catalogue, setCatalogue] = useState<Catalogue>();
 
   useEffect(() => {
-    setIsLoading(true);
-    axios
-      .get<Catalogue>(
-        "https://raw.githubusercontent.com/XiteTV/frontend-coding-exercise/main/data/dataset.json"
-      )
-      .then((res: AxiosResponse<Catalogue>) => {
-        console.log(res.data);
-        setIsLoading(false);
-        setCatalogue(res.data);
-      });
+    setTimeout(() => {
+      axios
+        .get<Catalogue>(
+          "https://raw.githubusercontent.com/XiteTV/frontend-coding-exercise/main/data/dataset.json"
+        )
+        .then((res: AxiosResponse<Catalogue>) => {
+          console.log(res.data);
+          setIsLoading(false);
+          setCatalogue(res.data);
+        })
+        .catch(() => alert("Error in fetching the catalogue"));
+    }, 2000);
   }, []);
 
-  return (
-    <div className={styles.pagesContainer}>
-      {isLoading ? (
-        <Spinner animation={"grow"} variant={"dark"} />
-      ) : (
-        catalogue?.videos.map((el) => {
+  const slicedCatalogue = useMemo(() => {
+    return catalogue?.videos.slice(
+      (currentPage - 1) * ITEMS_PER_PAGE,
+      currentPage * ITEMS_PER_PAGE
+    );
+  }, [catalogue, currentPage]);
+
+  return isLoading ? (
+    <CustomSpinner />
+  ) : (
+    <div className={styles.homeContainer}>
+      <CustomNavbar />
+      <div className={styles.catalogue}>
+        {slicedCatalogue?.map((el) => {
           return (
             <CustomCard
               key={el.id}
@@ -37,8 +52,14 @@ const Home = () => {
               title={el.title}
             />
           );
-        })
-      )}
+        })}
+      </div>
+      <CustomPagination
+        total={catalogue?.videos.length}
+        itemsPerPage={ITEMS_PER_PAGE}
+        currentPage={currentPage}
+        onChangePage={(page) => setCurrentPage(page)}
+      />
     </div>
   );
 };
